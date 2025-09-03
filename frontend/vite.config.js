7343bdd2c8df82aa63af3971a64b5f4f4051d193
Esiah Kapinga <esiahkapinga3@gmail.com>
2025-09-03 12:41:28 +0200

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: './', // assets load correctly in both local preview and deployed site
+  base: './', // ensures deployed assets load correctly
 })
