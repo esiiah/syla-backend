5811dfc4911ffb3ccbac8d1c8d510efb38cd7613
Esiah Kapinga <esiahkapinga3@gmail.com>
2025-09-18 13:40:55 +0200

    Update main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -5,7 +5,9 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import App from "./App.jsx";
 import FileToolPage from "./pages/FileToolPage.jsx"; // Page for individual tools
-import "./index.css";  // Tailwind styles
+import LoginPage from "./pages/LoginPage.jsx";
+import SignupPage from "./pages/SignupPage.jsx";
+import "./index.css"; // Tailwind styles
 
 import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
 
@@ -16,11 +18,18 @@ ReactDOM.createRoot(document.getElementById("root")).render(
         {/* Dashboard */}
         <Route path="/" element={<App />} />
 
+        {/* Authentication */}
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/signup" element={<SignupPage />} />
+
         {/* Dynamic tool pages */}
         <Route path="/tools/:action" element={<FileToolPage />} />
 
         {/* Catch-all for undefined routes */}
-        <Route path="*" element={<div className="p-6 text-center">Page Not Found</div>} />
+        <Route
+          path="*"
+          element={<div className="p-6 text-center">Page Not Found</div>}
+        />
       </Routes>
     </BrowserRouter>
   </React.StrictMode>
