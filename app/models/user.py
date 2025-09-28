b859583a7bdafef9dee4b933315dd5edc05ae1fd
Esiah Kapinga <esiahkapinga3@gmail.com>
2025-09-28 12:56:01 +0200

    Update user.py

--- app/models/user.py
+++ app/models/user.py
@@ -1,12 +1,3 @@
 # app/models/user.py
-
-"""
-Re-export the User model from app.routers.db to avoid duplicate table definitions.
-
-This prevents SQLAlchemy InvalidRequestError:
-  "Table 'users' is already defined for this MetaData instance."
-"""
-
 from app.routers.db import User
-
 __all__ = ["User"]
