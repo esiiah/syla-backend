ab6288c0932d53b0f76a44c6bdfdedfbd4440f31
Esiah Kapinga <esiahkapinga3@gmail.com>
2025-10-23 07:42:06 +0200

    Update tailwind.config.js

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,6 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./index.html", "./public/index.html", "./src/**/*.{js,jsx,ts,tsx,html}"],
+  important: true,
   darkMode: "class",
   theme: {
     extend: {
